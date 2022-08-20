6df6d2b1111db996f6b1a60583d361e1f262eacf
Wilmer Rivera <wilmerbaby@gmail.com>
2022-08-19 20:29:40 -0400

    first committ

---
+++ CatWorx.badgeMaker/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+          static List<string> GetEmployees() 
+          {
+          List<string> employees = new List<string>();
+          while (true)
+        {
+              Console.WriteLine("Please enter a name: ");
+      string input = Console.ReadLine();
+      employees.Add(input);
+       if(input == ""){
+      break;
+           }
+        employees.Add(input);
+        }
+    return employees;
+          }
+            static void PrintEmployees(List<string> employees)
+ 
+ {
+    for (int i = 0; i < employees.Count; i++)
+    {
+      Console.WriteLine(employees[i]);
+    }
+  }
+         static void Main(string[] args)
+    {
+    List<string> employees = GetEmployees();
+    PrintEmployees(employees);
+   }
+ }
+}
+
+
+
+   
+      
+
+ 
